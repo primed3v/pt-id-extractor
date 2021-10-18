8d1460e3db8670f1fb9aa5d4750a01660afcf737
primed3v <89787954+primed3v@users.noreply.github.com>
2021-10-18 12:08:42 +0100

    feat: regex cc updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,10 @@
 export const environment = {
     regex_nif : "\\d{9}",
-    regex_cc : "^[\\d]{8}[-, ]?\\d[-, ]?([A-Z]{2}\\d)?$",
-    regex_bi: "^[0-9]{7,8}[ ]?[0-9]$",
+    regex_cc : "[\\d]{8}[-, ]?[0-9]{1}[-, ]?([A-z]{2}\\d)",
+    regex_cc_letterorzero_mistake : "[\\d]{8}[-, ]?[A-z]{1}[-, ]?([A-z]{2}\\d)",
+    regex_cc_first_eigthnumbers : "[\\d]{8}[-, ]?",
+    regex_cc_last_3digits : "([A-z]{2}\\d)",
+    regex_bi: "[0-9]{7,8}[ ]?[0-9]",
     regex_niss: "[0-9]{11}",
     regex_passaportPt : "[A-Za-z][A-Za-z0-9]{6}[^A-Za-z0-9]",
     regex_passaportPt_2 : "\\d[A-Za-z0-9]{6}[^A-Za-z0-9]",
